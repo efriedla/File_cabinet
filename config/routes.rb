c804053c705c43c5df4c3a3e697b44de8c62e54f
efriedla <efriedla20852@gmail.com>
2019-05-28 13:32:55 -0700

    change root for authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
 
+  # Authorization
+  authenticated :user do 
+    root "docs#index", as: "authenticated_root"
+  end
+
   # landing page
   root 'welcome#index'
 
